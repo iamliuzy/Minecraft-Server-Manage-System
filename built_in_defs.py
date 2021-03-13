04efe162232aaa0cbc62974d6176257a1ee56593
Steve108 <59107734+iamliuzhiyu@users.noreply.github.com>
2021-03-13 21:52:58 +0800

    Delete built_in_defs.py

--- built_in_defs.py
+++
@@ -1,8 +0,0 @@
-import json
-from errorclass import MSMSError
-def get_settings(obj:str):
-    settingsf = open("settings.json")
-    settings = dict(json.loads(settingsf.read()))
-    settingsf.close()
-    r = str(settings[obj])
-    return r
