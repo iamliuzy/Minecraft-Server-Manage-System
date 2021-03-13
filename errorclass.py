3500623639b4e4eb4b3658ab7b3c308056eb19d5
Steve108 <59107734+iamliuzhiyu@users.noreply.github.com>
2021-03-13 21:53:06 +0800

    Delete errorclass.py

--- errorclass.py
+++
@@ -1,9 +0,0 @@
-class MSMSError(Exception):
-    """description of class"""
-    def __init__(self, name, code,description):
-        Exception.__init__(self)
-        self.name = name
-        self.code = code
-        self.description = description
-
-
