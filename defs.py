f4b7ace7e1019a6645afdb8ff9020c9710fd5e7a
Steve108 <59107734+iamliuzhiyu@users.noreply.github.com>
2021-03-13 21:53:13 +0800

    Delete defs.py

--- defs.py
+++
@@ -1,6 +0,0 @@
-from errorclass import MSMSError
-def parsting_command(command:str, commandlist:list):
-    command_head = str(command.split(" ")[0])
-    if not command_head in commandlist:
-        raise MSMSError("Command error", "2001", "Unkdown command:" + command)
-
