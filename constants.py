1dcde90013176a9b6c9511328b5b7dac6dfe912d
iamliuzhiyu <liuzhiyu.sh@outlook.com>
2021-03-28 18:28:34 +0800

    添加项目文件。

---
+++ constants.py
@@ -0,0 +1,9 @@
+VERSION = "0.0.1"
+COMMANDS_TO_FUNCTIONS = {"about":  "about()", "exit":  "exit()", "quit":  "exit()"}
+CRITICAL_ERROR_CODES = [0, 1000]
+SUPPORT_ERROR_CODES = [1000, 2001]
+COPYRIGHT = """
+Copyright (c) 2021 Shanghai Kouao Infotec Co., Ltd.
+License see https: //github.com/iamliuzhiyu/Minecraft-Server-Manage-System/blob/main/LICENSE.txt.
+            """
+
