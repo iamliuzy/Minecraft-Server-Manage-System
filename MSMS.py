dece79b54416e77afea68524345a7a26b14a55a3
Steve108 <liuzhiyu.sh@outlook.com>
2021-04-03 00:50:19 +0800

    Create _config.yml

--- MSMS.py
+++ MSMS.py
@@ -4,43 +4,46 @@ import os
 from sys import exit
 
 from constants import *
+class MSMS(object):
+    class Configuation(object):
+        def __init__(self, id:str, displayname:str, ver:str):
+            self.id = id
+            self.displayname = displayname
+            self.version = ver
+            self.info = {"id": id, "disname": displayname, "version": ver}
+            f = open(os.path.dirname(__file__).replace("/", "\\") + "\\configuations.json", "w", encoding="utf-8")
+            f.write(json.dumps(self.info))
+            f.close()
+        def edit(self, key:str, value:str):
+            self.info[key] = value
+            if key == "id":
+                self.id = value
+            if key == "displayname" or key == "disname":
+                self.displayname = value
+            if key == "ver" or key == "version":
+                self.version = value
 
-class Configuation(object):
-    def __init__(self, id:str, displayname:str, ver:str):
-        self.id = id
-        self.displayname = displayname
-        self.version = ver
-        self.info = {"id": id, "disname": displayname, "version": ver}
-        f = open(os.path.dirname(__file__).replace("/", "\\") + "\\configuations.json", "w", encoding="utf-8")
-        f.write(json.dumps(self.info))
-        f.close()
-    def edit(self, key:str, value:str):
-        self.info[key] = value
-        if key == "id":
-            self.id = value
-        if key == "displayname" or key == "disname":
-            self.displayname = value
-        if key == "ver" or key == "version":
-            self.version = value
 
+    def about(self):
+        print("Minecraft Server Manage System. Version " + VERSION)
+        print(COPYRIGHT)
 
-def about():
-    print("Minecraft Server Manage System. Version " + VERSION)
-    print(COPYRIGHT)
+    def parsting_command(self, command: str, commandlist: list):
+        command_head = str(command.split(" ")[0])
+        if command_head not in commandlist:
+            print("Unkdown command:" + command)
+        else:
+            eval(COMMANDS_TO_FUNCTIONS[command_head])
 
-def parsting_command(command: str, commandlist: list):
-    command_head = str(command.split(" ")[0])
-    if command_head not in commandlist:
-        print("Unkdown command:" + "command")
-    else:
-        eval(COMMANDS_TO_FUNCTIONS[command_head])
+    def __init__(self):
+        try:
+            print("Welcome to use Minecraft Server Manage System.")
+            print('Type "help" for more information.')
+            repeat = True
+            while repeat == True:
+                command = input(">")
+                self.parsting_command(command, COMMANDS_TO_FUNCTIONS)
+        except KeyboardInterrupt:
+            exit()
 
-try:
-    print("Welcome to use Minecraft Server Manage System.")
-    print('Type "help" for more information.')
-    repeat = True
-    while repeat == True:
-        command = input(">")
-        parsting_command(command, COMMANDS_TO_FUNCTIONS)
-except KeyboardInterrupt:
-    exit()
+MSMS()
--- constants.py
+++ constants.py
@@ -1,9 +1,6 @@
 VERSION = "0.0.1"
-COMMANDS_TO_FUNCTIONS = {"about":  "about()", "exit":  "exit()", "quit":  "exit()"}
+COMMANDS_TO_FUNCTIONS = {"about":  "self.about()", "exit":  "exit()", "quit":  "exit()"}
 CRITICAL_ERROR_CODES = [0, 1000]
 SUPPORT_ERROR_CODES = [1000, 2001]
-COPYRIGHT = """
-Copyright (c) 2021 Shanghai Kouao Infotec Co., Ltd.
-License see https: //github.com/iamliuzhiyu/Minecraft-Server-Manage-System/blob/main/LICENSE.txt.
-            """
+COPYRIGHT = "Copyright (c) 2021 Shanghai Kouao Infotec Co., Ltd.\nLicense: GPL v3.0\nLicense file: %s\\LICENSE.txt"
 
