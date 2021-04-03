c900bb1a2d7a4ece32c826dae3939f9091e631e1
Steve108 <liuzhiyu.sh@outlook.com>
2021-04-03 17:41:21 +0800

    Delete setproxy.bat

--- setproxy.bat
+++
@@ -1,9 +0,0 @@
-set http_proxy=http://127.0.0.1:8889
-
-set https_proxy=http://127.0.0.1:8889
-
-set http_proxy_user=qv2
-set http_proxy_pass=v2ray
-
-set https_proxy_user=qv2
-set https_proxy_pass=v2ray
