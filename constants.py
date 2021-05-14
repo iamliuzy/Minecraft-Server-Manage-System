71433ab9e43e4be54d2f9abb312600f7b8ad4205
Steve108 <liuzhiyu.sh@outlook.com>
2021-05-14 20:28:50 +0800

    Delete constants.py

--- constants.py
+++
@@ -1,6 +0,0 @@
-VERSION = "0.0.2_01"
-COMMANDS_TO_FUNCTIONS = {"about":  "self.about()", "exit":  "exit()", "quit":  "exit()"}
-CRITICAL_ERROR_CODES = [0, 1000]
-SUPPORT_ERROR_CODES = [1000, 2001]
-COPYRIGHT = "Copyright (c) 2021 Shanghai Kouao Infotec Co., Ltd.\nLicense: GPL v3.0\nLicense file: %s\\LICENSE.txt"
-
